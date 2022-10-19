b9580f69d242e547b087d70819c43d2c41a2795d
Lenovo 15 <information@com1software.com>
2022-10-19 13:03:37 -0400

    update

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"golang.org/x/exp/io/i2c"
 )
 
@@ -9,12 +11,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+	fmt.Printf("first return %s", d)
 	// opens a 10-bit address
 	d, err = i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, i2c.TenBit(0x78))
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("to bit return %s", d)
 
-	_ = d
 }
